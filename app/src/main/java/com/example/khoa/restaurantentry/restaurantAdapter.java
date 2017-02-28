ce33cb3f9d120f056dc5143f938e09afcc029a7c
Khoa Vo <Khoa Vo>
2017-02-27 22:14:49 -0600

    Initial commit

---
+++ app/src/main/java/com/example/khoa/restaurantentry/restaurantAdapter.java
@@ -0,0 +1,60 @@
+package com.example.khoa.restaurantentry;
+
+import java.util.List;
+import java.util.Objects;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class restaurantAdapter extends BaseAdapter  {
+    private Context listContext;
+    private List<restaurantData> listRestaurant;
+
+    public restaurantAdapter(Context context, List<restaurantData> list)
+    {
+        listContext = context;
+        listRestaurant = list;
+    }
+
+    @Override
+    public int getCount()
+    {
+        return listRestaurant.size();
+    }
+
+    @Override
+    public Object getItem(int pos)
+    {
+        return listRestaurant.get(pos);
+    }
+
+    @Override
+    public long getItemId(int pos){
+        return pos;
+    }
+
+    @Override
+    public View getView(int pos, View convertView, ViewGroup parent){
+        restaurantData entry = listRestaurant.get(pos);
+
+        ImageView pictures = (ImageView) convertView.findViewById(R.id.getImage);
+        pictures.setImageBitmap(entry.getPicture());
+
+        TextView tvName = (TextView)convertView.findViewById(R.id.restName);
+        tvName.setText(entry.getName());
+
+        TextView tvCity = (TextView)convertView.findViewById(R.id.cityName);
+        tvCity.setText(entry.getCity());
+
+        return convertView;
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/khoa/restaurantentry/restaurantData.java
@@ -0,0 +1,39 @@
+package com.example.khoa.restaurantentry;
+import android.graphics.Bitmap;
+
+
+public class restaurantData
+{
+    private Bitmap Picture;
+    private String Name;
+    private String City;
+
+    public restaurantData(Bitmap picture, String name, String city){
+        Picture = picture;
+        Name = name;
+        City = city;
+    }
+
+    public void setPicture(Bitmap picture) {
+        Picture = picture;
+    }
+    public Bitmap getPicture() {
+        return Picture;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+}
